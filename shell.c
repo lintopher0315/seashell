097389d19875534458b17e0c738b1c7124aeb77e
lintopher0315 <lin978@purdue.edu>
2019-03-08 19:15:06 -0500

    prompts user for input and prints it out

--- shell.c
+++ shell.c
@@ -1,7 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <readline/readline.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 int main() {
+
+    char *input = "";
+
+    while(1) {
+        input = readline("seashell> ");
+        printf("%s\n", input);
+    }
 
     return 0;
 }
