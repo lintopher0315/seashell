b6c34b96aa9833fafc00701ff722413c51bffc13
lintopher0315 <lin978@purdue.edu>
2019-03-10 15:44:05 -0400

    parses basic input and commands

--- shell.c
+++ shell.c
@@ -5,14 +5,48 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+char **parse_input(char *);
+
 int main() {
 
     char *input = "";
+    char **commands;
+    pid_t child;
+    int child_stat = 0;
 
-    while(1) {
+    while (1) {
         input = readline("seashell> ");
-        printf("%s\n", input);
+        commands = parse_input(input);
+
+        child = fork();
+
+        if (child == 0) {
+            execvp(commands[0], commands);
+        }
+        else {
+            waitpid(child, &child_stat, WUNTRACED);
+        }
+
+        free(input);
+        free(commands);
     }
 
     return 0;
+}
+
+char **parse_input(char *input) {
+    char **args = malloc(16 * sizeof(char *));
+    char *token;
+    int element = 0;
+
+    token = strtok(input, " ");
+
+    while (token != NULL) {
+        args[element] = token;
+        element++;
+        token = strtok(NULL, " ");
+    }
+    args[element] = NULL;
+
+    return args;
 }
