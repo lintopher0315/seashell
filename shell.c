1b1e09ff69118db74aff05da494a9e27e75aa58d
lintopher0315 <lin978@purdue.edu>
2019-03-12 20:28:14 -0400

    reads in history file into shell

--- shell.c
+++ shell.c
@@ -7,11 +7,46 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+char **history;
+int history_size = 0;
+
 int main() {
 
     char *input = "";
     char **commands;
     int command_ret = 0;
+
+    FILE *fp = NULL;
+
+    history = calloc(MAX_HISTORY * sizeof(char *), 1);
+
+    if (access("seashell_history.txt", F_OK) != -1) {
+        fp = fopen("seashell_history.txt", "ab+");
+        if (fp == NULL) {
+            printf("couldn't access history\n");
+            free(history);
+            return 0;
+        }
+        
+        if (read_in_history(fp) != 0) {
+            printf("couldn't read in history\n");
+            fclose(fp);
+            fp = NULL;
+            free(history);
+            return 0;
+        }
+    }
+    else {
+        fp = fopen("seashell_history.txt", "ab+");
+        if (fp == NULL) {
+            printf("couldn't access history\n");
+            free(history);
+            return 0;
+        }
+    }
+
+    fclose(fp);
+    fp = NULL;
 
     while (1) {
         input = readline("seashell> ");
@@ -29,11 +64,13 @@ int main() {
         free(commands);
     }
 
+    free(history);
+
     return 0;
 }
 
 char **parse_input(char *input) {
-    char **args = malloc(16 * sizeof(char *));
+    char **args = malloc(MAX_ARGS * sizeof(char *));
     if (args == NULL) {
         perror("malloc call unsuccessful");
         exit(1);
@@ -85,6 +122,31 @@ int handle_command(char **input) {
         else {
             waitpid(child, &child_stat, WUNTRACED);
         }
+    }
+    return 0;
+}
+
+int read_in_history(FILE *file_pointer) {
+    int scan_val = 0;
+    int num_items = 0;
+    int history_loc = 0;
+    char cmd[MAX_ARG_LENGTH];
+
+    if (fscanf(file_pointer, "%d\n", &num_items) == 1) {
+        if (num_items > MAX_HISTORY) {
+            num_items = MAX_HISTORY;
+        }
+        history_size = num_items;
+    }
+    while (((scan_val = fscanf(file_pointer, "%s\n", cmd)) != EOF) && history_loc < MAX_HISTORY) {
+        if (scan_val == 1) {
+            history[history_loc] = malloc(MAX_ARG_LENGTH * sizeof(char));
+            strcpy(history[history_loc], cmd);
+        }
+        else {
+            return -1;
+        }
+        history_loc++;
     }
     return 0;
 }
--- shell.h
+++ shell.h
@@ -1,6 +1,16 @@
-#define MAX_ARGS    1024
-#define MAX_HISTORY 1000
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+
+#define MAX_ARGS        1024
+#define MAX_HISTORY     10
+#define MAX_ARG_LENGTH  50
 
 char **parse_input(char *);
 
 int handle_command(char **);
+
+int read_in_history(FILE *);
+
+#endif
